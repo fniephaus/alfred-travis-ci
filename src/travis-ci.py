af12815abcfb2893fa5abddc7a16e3ecb50b6f1e
Fabio Niephaus <code@fniephaus.com>
2017-08-09 10:56:24 -0700

    Improve workflow

--- src/travis-ci.py
+++ src/travis-ci.py
@@ -1,43 +1,57 @@
+import datetime
 import sys
-from urllib2 import urlopen, HTTPError
-import json
+import humanize
 from workflow import Workflow, web
 
-API_BASE_URL = 'https://api.travis-ci.org/'
+API_BASE_URL = 'https://api.travis-ci.org'
+
+
+def display_repo(user_input):
+    req = web.get('%s/repos/%s' % (API_BASE_URL, user_input))
+    try:
+        repo = req.json()
+    except:
+        parts = user_input.split('/')
+        return display_repos(parts[0], parts[1])
+    status = get_status(repo['last_build_result'])
+    title = 'Build #%s (%s)' % (repo['last_build_number'], status)
+    timedelta = datetime.timedelta(seconds=repo['last_build_duration'])
+    subtitle = 'Duration: %s' % humanize.naturaldelta(timedelta)
+    wf.add_item(title, subtitle,
+                arg=user_input, autocomplete=user_input, valid=True)
+
+
+def display_repos(user_input, repo_prefix=None):
+    req = web.get('%s/repos/%s' % (API_BASE_URL, user_input))
+    if req.status_code != 200:
+        return wf.add_item('User invalid.', arg='')
+    repos = req.json()
+    if len(repos) > 0:
+        for repo in repos:
+            if repo_prefix:
+                repo_name = repo['slug'].split('/')[1]
+                if not repo_name.startswith(repo_prefix):
+                    continue
+            wf.add_item(repo['slug'], repo['description'], arg=repo[
+                        'slug'], autocomplete=repo['slug'], valid=True)
+    else:
+        wf.add_item('No repositories found for this user.', arg='')
 
 
 def main(wf):
     user_input = ''.join(wf.args)
-    if "/" in user_input:
-        try:
-            repo = web.get(API_BASE_URL + 'repos/' + user_input).json()
-            status = get_status(repo['last_build_result'])
-            title = 'Build #' + \
-                str(repo['last_build_number']) + ' (' + status + ')'
-            subtitle = 'Duration: ' + str(repo['last_build_duration'])
-            wf.add_item(title, subtitle,
-                        arg=user_input, autocomplete=user_input, valid=True)
-        except HTTPError:
-            wf.add_item('Slug invalid.', arg='')
+    if not user_input:
+        wf.add_item('Enter a username or a repo slug.', arg='')
+    elif '/' in user_input:
+        display_repo(user_input)
     else:
-        try:
-            repos = web.get(API_BASE_URL + 'repos/' + user_input).json()
-            if len(repos) > 0:
-                for repo in repos:
-                    wf.add_item(repo['slug'], repo['description'], arg=repo[
-                                'slug'], autocomplete=repo['slug'], valid=True)
-            else:
-                wf.add_item('No repositories found for this user.', arg='')
-        except HTTPError:
-            wf.add_item('User invalid.', arg='')
+        display_repos(user_input)
 
     wf.send_feedback()
 
 
 def get_status(res):
-    if res == 0:
-        return "Passed"
-    return "Failed"
+    return 'Passed' if res == 0 else 'Failed'
 
 
 if __name__ == '__main__':
